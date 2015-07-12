8ff405cd294e7ea5e4d0749eab7910eee8f84d35
dylan <dylan.drost@gmail.com>
2015-07-12 17:04:40 +0200

    added Toolbar sample to the sample app

--- app/src/main/java/com/pixplicity/fontview/sample/MainActivity.java
+++ app/src/main/java/com/pixplicity/fontview/sample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.pixplicity.fontview.sample;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayShowTitleEnabled(false);
+        }
     }
 }
